892fe70ec98d309b492248e85643d32581a549cb
antinz <antoninzhirov@gmail.com>
2025-05-02 19:22:19 +0300

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </head>
       <body className={`${outfit.variable}`}>{children}</body>
     </html>
   );
