68a2f3280d3fdc37d65ae5dc28895d70f35f54ed
antinz <antoninzhirov@gmail.com>
2025-06-17 15:27:45 +0300

    adjusted a bit lazy loading

--- Components/LazyBlogItemWrapper.jsx
+++ Components/LazyBlogItemWrapper.jsx
@@ -13,7 +13,16 @@ const LazyBlogItemWrapper = ({ item }) => {
     <div ref={ref}>
       {inView && (
         <Suspense
-          fallback={<div className="h-64 bg-gray-100 rounded">Loading...</div>}
+          fallback={
+            <div className="animate-pulse rounded shadow p-4 border border-gray-200 h-64 flex flex-col gap-4 bg-white">
+              <div className="bg-gray-200 h-32 w-full rounded" />{" "}
+              {/* image placeholder */}
+              <div className="h-4 bg-gray-200 rounded w-3/4" />{" "}
+              {/* title placeholder */}
+              <div className="h-3 bg-gray-200 rounded w-full" /> {/* line 1 */}
+              <div className="h-3 bg-gray-200 rounded w-5/6" /> {/* line 2 */}
+            </div>
+          }
         >
           <LazyBlogItem {...item} />
         </Suspense>
